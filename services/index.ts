d0393b10b6eace890c15fa07598e1d26eb6e2803
ihsankcck <96536577+ihsankcck@users.noreply.github.com>
2022-06-30 04:12:43 +0300

    update

--- services/index.ts
+++ services/index.ts
@@ -2,7 +2,7 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import axios from 'axios';
 
 const Api = axios.create({
-  baseURL: 'http://192.168.1.60:1337/api',
+  baseURL: 'https://cms.gallant-wiles.37-140-242-109.plesk.page',
 });
 
 Api.interceptors.request.use(
